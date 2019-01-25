449f5770d85744fdfdd3840b8af03161e6c00ca7
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-01-25 19:59:09 +0900

    [add]refs #24 プレイヤーの発見位置によって敵の瞬間移動位置を変える

--- TransmigrateActionGame/Assets/Scripts/EnemyController.cs
+++ TransmigrateActionGame/Assets/Scripts/EnemyController.cs
@@ -7,6 +7,8 @@ public class EnemyController : MonoBehaviour {
     public int enemyPattern;
     public float switchSpeed;
 
+    public float attackInterval;
+    public float vanishSpeed;
 
     Animator enemyAnimator;
     RaycastHit2D hit;
@@ -14,6 +16,22 @@ public class EnemyController : MonoBehaviour {
     PlayerController playerController;
     GameObject player;
 
+    enum DIRECTION
+    {
+        NONE = -1,
+
+        FRONT = 0,
+        BACK,
+        LEFT,
+        RIGHT,
+
+        NUM
+    }
+
+    DIRECTION findPos = DIRECTION.NONE;
+    DIRECTION enemyEyeDirection = DIRECTION.NONE;
+
+    IEnumerator switchCoroutine;
 
     // TODO 最終的にはStartではなく、ステージアクティブになったら以下の処理を行う
     void Start ()
@@ -21,28 +39,44 @@ public class EnemyController : MonoBehaviour {
         playerController = FindObjectOfType<PlayerController>();
         player = GameObject.Find("Player");
 
-        enemyAnimator = GetComponent<Animator>();
+        enemyAnimator = GetComponent<Animator>();        
 
         switch (enemyPattern)
         {
             case 1:
-                StartCoroutine(SwitchDirectionPattern1());
+                switchCoroutine = SwitchDirectionPattern1();
+                StartCoroutine(switchCoroutine);
                 break;
         }
     }
 
+    private DIRECTION DetermineFindPos()
+    {
+        float y = player.transform.position.y - transform.position.y;
+        float x = player.transform.position.x - transform.position.x;
+        float degree = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+
+        if (degree > -135f && degree < -45f) { return DIRECTION.FRONT; }
+
+        if (degree > -45f && degree < 45f) { return DIRECTION.RIGHT; }
+
+        if (degree > 45f && degree < 135f) { return DIRECTION.BACK; }
+
+        if (degree > 135f && degree > -135f) { return DIRECTION.LEFT; }
+
+        return DIRECTION.NONE;
+    }
+
     private void Update()
     {
-
         // ゲームステージ中のみ敵の動きを実行
         // Update内でステージ状況監視する以外思いつかなかった...
-        if (!playerController.isInStage) { StopAllCoroutines(); }
-
+        if (!playerController.isInStage) { StopCoroutine(switchCoroutine); }
 
         // 敵に見つかったらゲームオーバー
         if (hit && hit.collider.CompareTag("Player") && playerController.isInStage)
         {
-            Attack();
+            StartCoroutine(Teleportation());
         }
 
         // 敵にぶつかってもゲームオーバー
@@ -51,6 +85,27 @@ public class EnemyController : MonoBehaviour {
             Attack();
         }
     }
+
+    private void FixedUpdate()
+    {
+        // Update内でレイキャストを飛ばさないとレイキャスト持続しない
+        switch (enemyEyeDirection)
+        {
+            case DIRECTION.FRONT:
+                hit = Physics2D.Raycast(transform.position, Vector2.down);
+                break;
+            case DIRECTION.BACK:
+                hit = Physics2D.Raycast(transform.position, Vector2.up);
+                break;
+            case DIRECTION.RIGHT:
+                hit = Physics2D.Raycast(transform.position, Vector2.right);
+                break;
+            case DIRECTION.LEFT:
+                hit = Physics2D.Raycast(transform.position, Vector2.left);
+                break;
+        }
+    }
+
 
     // TODO コルーチンでなくUpdate()内で動かせたら理想
     public IEnumerator SwitchDirectionPattern1()
@@ -76,42 +131,68 @@ public class EnemyController : MonoBehaviour {
 
     void TurnFront()
     {
-        hit = Physics2D.Raycast(transform.position, Vector2.down);
-        Debug.Log("front");
+        enemyEyeDirection = DIRECTION.FRONT;
         enemyAnimator.SetTrigger("TurnFront");
     }
 
     void TurnBack()
     {
-        hit = Physics2D.Raycast(transform.position, Vector2.up);
-        Debug.Log("back");
+        enemyEyeDirection = DIRECTION.BACK;
         enemyAnimator.SetTrigger("TurnBack");
     }
 
     void TurnRight()
     {
-        hit = Physics2D.Raycast(transform.position, Vector2.right);
-        Debug.Log("right");
+        enemyEyeDirection = DIRECTION.RIGHT;
         enemyAnimator.SetTrigger("TurnRight");
     }
 
     void TurnLeft()
     {
-        hit = Physics2D.Raycast(transform.position, Vector2.left);
-        Debug.Log("left");
+        enemyEyeDirection = DIRECTION.LEFT;
         enemyAnimator.SetTrigger("TurnLeft");
+    }
+
+    IEnumerator Teleportation()
+    {
+        playerController.isInStage = false;
+        yield return new WaitForSeconds(attackInterval);
+
+        // 一瞬消える
+        yield return new WaitForSeconds(1f);
+        
+        findPos = DetermineFindPos();
+        MovePlayerSide();
+
+        Debug.Log("game over");
+        yield break;
+
     }
 
     void Attack()
     {
         playerController.isInStage = false;
         Debug.Log("game over");
-
     }
 
     void MovePlayerSide()
     {
-
+        // プレイヤーを発見した方向によって瞬間移動位置を変える
+        switch (findPos)
+        {
+            case DIRECTION.FRONT:
+                transform.position = new Vector3(player.transform.position.x, player.transform.position.y + playerController.playerRadius * 2f, transform.position.z);
+                break;
+            case DIRECTION.BACK:
+                transform.position = new Vector3(player.transform.position.x, player.transform.position.y - playerController.playerRadius * 2f, transform.position.z);
+                break;
+            case DIRECTION.RIGHT:
+                transform.position = new Vector3(player.transform.position.x - playerController.playerRadius * 2f, player.transform.position.y, transform.position.z);
+                break;
+            case DIRECTION.LEFT:
+                transform.position = new Vector3(player.transform.position.x + playerController.playerRadius * 2f, player.transform.position.y, transform.position.z);
+                break;
+        }
     }
 
 }
