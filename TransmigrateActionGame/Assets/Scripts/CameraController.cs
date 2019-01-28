67cf392766362448a5e296b9215e69726d466499
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-01-28 16:06:25 +0900

    [add]refs #31 ステージ移動の際はカメラが一緒に移動するように

---
+++ TransmigrateActionGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    StageDirector stageDirector;
+
+    public GameObject player;
+	
+	void Start () {
+        stageDirector = FindObjectOfType<StageDirector>();
+	}
+	
+	
+	void Update () {
+		if(stageDirector.stageState == StageDirector.STAGESTATE.MOVE)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
