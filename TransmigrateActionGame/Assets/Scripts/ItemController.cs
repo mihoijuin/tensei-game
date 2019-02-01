cce65cc86b3abccb9a6fda3d16b66341b9573203
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-02-01 14:57:16 +0900

    [add]refs #50 アイテムを明滅

---
+++ TransmigrateActionGame/Assets/Scripts/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+
+    float opacity;
+    public float blinkSpeed;
+    public float blinkOffset;
+    public float minOpacity;
+
+    SpriteRenderer itemRenderer;
+    Color originColor;
+
+    StageDirector stageDirector;
+	
+	void Start () {
+        stageDirector = FindObjectOfType<StageDirector>();
+        itemRenderer = GetComponent<SpriteRenderer>();
+        originColor = itemRenderer.color;
+    }
+	
+	
+	void Update () {
+        if(stageDirector.stageState == StageDirector.STAGESTATE.INSTAGE)
+        {
+            opacity = Mathf.PingPong(Time.time * blinkSpeed, blinkOffset) + minOpacity;
+            itemRenderer.color = new Color(originColor.r, originColor.g, originColor.b, opacity);
+        }
+    }
+}
